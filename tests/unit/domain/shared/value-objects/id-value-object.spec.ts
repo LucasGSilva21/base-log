1dbc8ae3190fd07c4e39b6b0305e8547286f3d3e
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-09-27 13:41:01 -0300

    test: ensure instantiate a new id without parameter

--- tests/unit/domain/shared/value-objects/id-value-object.spec.ts
+++ tests/unit/domain/shared/value-objects/id-value-object.spec.ts
@@ -1,12 +1,19 @@
 import { Id } from '../../../../../src/domain/shared/value-objects';
 import { v4 as uuidv4 } from 'uuid';
 
-describe('Base Entity', () => {
+describe('Id Value Object', () => {
   test('should instantiate a new id with a valid uuid in parameter', () => {
     const uuid = uuidv4();
     const id = new Id(uuid);
     expect(id).toBeDefined();
     const getValue = id.getValue();
     expect(getValue).toBe(uuid);
+  });
+
+  test('should instantiate a new id without parameter', () => {
+    const id = new Id();
+    expect(id).toBeDefined();
+    const getValue = id.getValue();
+    expect(getValue).toBeDefined();
   });
 });
