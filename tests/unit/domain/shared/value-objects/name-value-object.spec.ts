88fb8802a887b06a038c7d91c87f779a3624da54
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-09-28 13:25:33 -0300

    test: add tests to Name value object

---
+++ tests/unit/domain/shared/value-objects/name-value-object.spec.ts
@@ -0,0 +1,17 @@
+import { Name } from '../../../../../src/domain/shared/value-objects';
+import { DomainError } from '../../../../../src/domain/shared/errors';
+
+describe('Name Value Object', () => {
+  test('should instantiate a new Name with a valid parameter', () => {
+    const validName = 'Valid Test';
+    const name = new Name(validName);
+    expect(name).toBeDefined();
+    const getValue = name.getValue();
+    expect(getValue).toBe(validName);
+  });
+
+  test('should throw a DomainError if send a invalid name', () => {
+    const invalidName = 'invalid name 123';
+    expect(() => new Name(invalidName)).toThrow(DomainError);
+  });
+});
