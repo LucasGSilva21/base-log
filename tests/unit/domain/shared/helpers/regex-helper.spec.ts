e87a7341f579c3cd866dad9ee1b20967da5ad796
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-09-29 14:54:13 -0300

    test: add validate password

--- tests/unit/domain/shared/helpers/regex-helper.spec.ts
+++ tests/unit/domain/shared/helpers/regex-helper.spec.ts
@@ -1,4 +1,4 @@
-import { validateUserName, validateEmail } from '../../../../../src/domain/shared/helpers';
+import { validateUserName, validateEmail, validatePassword } from '../../../../../src/domain/shared/helpers';
 
 describe('ValidateUserName', () => {
   test('should return true if the value contains only letters and spaces', () => {
@@ -48,6 +48,38 @@ describe('ValidateEmail', () => {
   test('should return false if the value is a invalid email', () => {
     const field = 'invalid-email';
     const isValid = validateEmail(field);
+    expect(isValid).toBe(false);
+  });
+});
+
+describe('ValidatePassword', () => {
+  test('should return true if the value is a valid password', () => {
+    const field = '#Valid123#';
+    const isValid = validatePassword(field);
+    expect(isValid).toBe(true);
+  });
+
+  test('should return false if the value is a empty string', () => {
+    const field = '';
+    const isValid = validatePassword(field);
+    expect(isValid).toBe(false);
+  });
+
+  test('should return false if the value not has a least one upper case letter', () => {
+    const field = 'invalid-password';
+    const isValid = validatePassword(field);
+    expect(isValid).toBe(false);
+  });
+
+  test('should return false if the value not has a least one lower case letter', () => {
+    const field = '#INVALID#';
+    const isValid = validatePassword(field);
+    expect(isValid).toBe(false);
+  });
+
+  test('should return false if the value not has a least one special character', () => {
+    const field = 'invalidPassword';
+    const isValid = validatePassword(field);
     expect(isValid).toBe(false);
   });
 });
