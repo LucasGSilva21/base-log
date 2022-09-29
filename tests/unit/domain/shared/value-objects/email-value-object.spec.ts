2e804f827ff244b23fcc341b8958d82b6ecc38af
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-09-29 14:11:34 -0300

    test: add Email value object

---
+++ tests/unit/domain/shared/value-objects/email-value-object.spec.ts
@@ -0,0 +1,19 @@
+
+import { Email } from '../../../../../src/domain/shared/value-objects';
+import { DomainError } from '../../../../../src/domain/shared/errors';
+
+describe('Email Value Object', () => {
+  test('should instantiate a new Email with a valid parameter', () => {
+    const validEmail = 'valid@email.com';
+    const email = new Email(validEmail);
+    expect(email).toBeDefined();
+    const getValue = email.getValue();
+    expect(getValue).toBe(validEmail);
+  });
+
+  test('should throw a DomainError if send a invalid email', () => {
+    const invalidEmail = 'invalid-email';
+    expect(() => new Email(invalidEmail)).toThrow(DomainError);
+    expect(() => new Email(invalidEmail)).toThrow(`The email ${invalidEmail} is invalid`);
+  });
+});
