967e35af60c5460fe4bdfb5d0988f9d2fcc7ffd8
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-09-29 14:11:23 -0300

    feat: add Email value object

---
+++ src/domain/shared/value-objects/email-value-object.ts
@@ -0,0 +1,23 @@
+import { ValueObject } from '../protocols';
+import { DomainError } from '../errors';
+import { validateEmail } from '../helpers';
+
+export class Email implements ValueObject<string> {
+  private _email: string;
+
+  constructor(email: string) {
+    this.validate(email);
+    this._email = email;
+  }
+
+  getValue(): string {
+    return this._email;
+  }
+
+  private validate(email: string): void {
+    const isValid = validateEmail(email);
+    if (!isValid) {
+      throw new DomainError(`The email ${email} is invalid`);
+    }
+  }
+}
--- src/domain/shared/value-objects/index.ts
+++ src/domain/shared/value-objects/index.ts
@@ -1,2 +1,3 @@
+export * from './email-value-object';
 export * from './id-value-object';
 export * from './user-name-value-object';
