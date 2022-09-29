214eec388583f2a3414b2324046f53a5296e6a22
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-09-29 20:54:56 -0300

    feat: add Password value object

--- src/domain/shared/value-objects/index.ts
+++ src/domain/shared/value-objects/index.ts
@@ -1,3 +1,4 @@
 export * from './email-value-object';
 export * from './id-value-object';
+export * from './password-value-object';
 export * from './user-name-value-object';
---
+++ src/domain/shared/value-objects/password-value-object.ts
@@ -0,0 +1,42 @@
+import { ValueObject } from '../protocols';
+import { DomainError } from '../errors';
+import { validatePassword, hash, compare } from '../helpers';
+
+interface PasswordInput {
+  password?: string
+  passwordHash?: string
+}
+
+export class Password implements ValueObject<string> {
+  private _passwordHash: string;
+
+  constructor({
+    password,
+    passwordHash
+  }: PasswordInput) {
+    if (!password && !passwordHash) {
+      throw new DomainError('A password or a password hash is required');
+    }
+    if (passwordHash) {
+      this._passwordHash = passwordHash;
+    } else {
+      this.validate(password);
+      this._passwordHash = hash(password);
+    }
+  }
+
+  getValue(): string {
+    return this._passwordHash;
+  }
+
+  async comparePassword (value: string): Promise<boolean> {
+    return compare(value, this._passwordHash);
+  }
+
+  private validate(password: string): void {
+    const isValid = validatePassword(password);
+    if (!isValid) {
+      throw new DomainError(`The password ${password} is invalid`);
+    }
+  }
+}
