ea2ae1e2cea03ab92173349f317e86126a6ef28c
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-09-28 23:06:01 -0300

    feat: add validateEmail

--- src/domain/shared/helpers/regex-helper.ts
+++ src/domain/shared/helpers/regex-helper.ts
@@ -1,3 +1,7 @@
 export const validateUserName = (value: string): boolean => {
   return /(?=^([^0-9]*)$)(?=(?!^ +$)^.+$)/.test(value);
 };
+
+export const validateEmail = (value: string): boolean => {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value);
+};
