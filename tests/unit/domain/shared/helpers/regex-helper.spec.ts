469b84f81c8db01ab8d1b7bd4c4d792b72d5cc65
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-09-28 23:06:11 -0300

    test: add validateEmail

--- tests/unit/domain/shared/helpers/regex-helper.spec.ts
+++ tests/unit/domain/shared/helpers/regex-helper.spec.ts
@@ -1,6 +1,6 @@
-import { validateUserName } from '../../../../../src/domain/shared/helpers';
+import { validateUserName, validateEmail } from '../../../../../src/domain/shared/helpers';
 
-describe('ValidateUserName Helper', () => {
+describe('ValidateUserName', () => {
   test('should return true if the value contains only letters and spaces', () => {
     const field = 'Valid Name';
     const isValid = validateUserName(field);
@@ -22,6 +22,32 @@ describe('ValidateUserName Helper', () => {
   test('should return false if the value contains numbers', () => {
     const field = 'invalid123';
     const isValid = validateUserName(field);
+    expect(isValid).toBe(false);
+  });
+});
+
+describe('ValidateEmail', () => {
+  test('should return true if the value is a valid email', () => {
+    const field = 'valid@email.com';
+    const isValid = validateEmail(field);
+    expect(isValid).toBe(true);
+  });
+
+  test('should return true if the value is a valid email without dot', () => {
+    const field = 'valid@email';
+    const isValid = validateEmail(field);
+    expect(isValid).toBe(true);
+  });
+
+  test('should return false if the value is a empty string', () => {
+    const field = '';
+    const isValid = validateEmail(field);
+    expect(isValid).toBe(false);
+  });
+
+  test('should return false if the value is a invalid email', () => {
+    const field = 'invalid-email';
+    const isValid = validateEmail(field);
     expect(isValid).toBe(false);
   });
 });
