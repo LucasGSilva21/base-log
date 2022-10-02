77327e7b886bce3ea96ff4d251233f225812cdc3
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-10-01 23:14:09 -0300

    test: add tests to account entity

---
+++ tests/unit/domain/entities/account.spec.ts
@@ -0,0 +1,87 @@
+import { AccountEntity } from '../../../../src/domain/entities';
+import { Id, UserName, Email, Password } from '../../../../src/domain/shared/value-objects';
+import { generateUuid } from '../../../../src/domain/shared/helpers';
+import MockDate from 'mockdate';
+
+jest.mock('bcrypt', () => ({
+  hashSync (): string {
+    return 'hash';
+  },
+
+  async compare (): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+}));
+
+describe('Account Entity', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test('should instantiate a new AccountEntity', () => {
+    const name = new UserName('Valid Name');
+    const email = new Email('valid@email.com');
+    const password = new Password({ password: '#Valid123#' });
+    const account = new AccountEntity({ name, email, password });
+    expect(account.id).toBeDefined();
+    expect(account.name).toBe(name);
+    expect(account.email).toBe(email);
+    expect(account.isActive).toBe(false);
+    expect(account.createdAt).toBeDefined();
+    expect(account.updatedAt).toBeDefined();
+  });
+
+  test('should instantiate a new AccountEntity with all fields', () => {
+    const uuid = generateUuid();
+    const id = new Id(uuid);
+    const name = new UserName('Valid Name');
+    const email = new Email('valid@email.com');
+    const password = new Password({ password: '#Valid123#' });
+    const isActive = true;
+    const createdAt = new Date();
+    const updatedAt = new Date();
+    const account = new AccountEntity({
+      id, name, email, password, isActive, createdAt, updatedAt
+    });
+    expect(account.id).toBe(id);
+    expect(account.name).toBe(name);
+    expect(account.email).toBe(email);
+    expect(account.isActive).toBe(true);
+    expect(account.createdAt).toBe(createdAt);
+    expect(account.updatedAt).toBe(updatedAt);
+  });
+
+  test('should return true when compare succeds', async () => {
+    const name = new UserName('Valid Name');
+    const email = new Email('valid@email.com');
+    const password = new Password({ password: '#Valid123#' });
+    const account = new AccountEntity({ name, email, password });
+    const isValid = await account.comparePassword('any_value');
+    expect(isValid).toBe(true);
+  });
+
+  test('should return correct values when call mapper', async () => {
+    const uuid = generateUuid();
+    const id = new Id(uuid);
+    const name = new UserName('Valid Name');
+    const email = new Email('valid@email.com');
+    const password = new Password({ password: '#Valid123#' });
+    const isActive = true;
+    const createdAt = new Date();
+    const updatedAt = new Date();
+    const account = new AccountEntity({
+      id, name, email, password, isActive, createdAt, updatedAt
+    });
+    const accountPrimitivesProps = await account.mapperToPrimitives();
+    expect(accountPrimitivesProps.id).toBe(id.getValue());
+    expect(accountPrimitivesProps.name).toBe(name.getValue());
+    expect(accountPrimitivesProps.email).toBe(email.getValue());
+    expect(accountPrimitivesProps.isActive).toBe(true);
+    expect(accountPrimitivesProps.createdAt).toBe(createdAt);
+    expect(accountPrimitivesProps.updatedAt).toBe(updatedAt);
+  });
+});
