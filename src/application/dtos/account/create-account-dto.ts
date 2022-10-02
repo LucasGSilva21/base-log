549665efcb7caa67b84fc1100cabdc604efdafb5
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-10-02 17:30:33 -0300

    feat: add account dtos

---
+++ src/application/dtos/account/create-account-dto.ts
@@ -0,0 +1,6 @@
+export interface CreateAccountDto {
+  userName: string
+  email: string
+  password: string
+  isActive?: boolean
+}
---
+++ src/application/dtos/account/result-account-dto.ts
@@ -0,0 +1,8 @@
+export interface ResultAccountDto {
+  id: string
+  userName: string
+  email: string
+  isActive: boolean
+  createdAt: Date
+  updatedAt: Date
+}
