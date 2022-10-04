7e0f015cd7f0cdfc697afaeea3b31fac026f10b1
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-10-03 21:42:32 -0300

    feat: add create account usecase

---
+++ src/application/usecases/account/create-account-usecase.ts
@@ -0,0 +1,29 @@
+import { AccountEntity } from '../../../domain/entities';
+import { UserName, Email, Password } from '../../../domain/shared/value-objects';
+import { UseCase } from '../../shared/protocols';
+import { CreateAccountDto, ResultAccountDto } from '../../dtos/account';
+import { CreateAccountRepository } from '../../repositories/account';
+
+export class CreateAccountUseCase implements UseCase<CreateAccountDto, ResultAccountDto> {
+  constructor (
+    private readonly createAccountRepository: CreateAccountRepository
+  ) {}
+
+  async exec (data: CreateAccountDto): Promise<ResultAccountDto> {
+    const userName = new UserName(data.userName);
+    const email = new Email(data.email);
+    const password = new Password({ password: data.password });
+    const isActive = data.isActive;
+
+    const account = new AccountEntity({
+      userName,
+      email,
+      password,
+      isActive
+    });
+
+    const accountCreated = await this.createAccountRepository.create(account);
+
+    return accountCreated.mapperToPrimitives();
+  }
+}
