b07be97f67966233971559cc023a981a3d10bf84
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-11-02 17:00:47 -0300

    feat: add SignUp Controller

---
+++ src/presentation/controllers/authentication/sign-up-controller.ts
@@ -0,0 +1,23 @@
+import { ErrorBase } from '@domain/shared/protocols';
+import { UseCase } from '@application/shared/protocols';
+import { SignUpInputDto, SignUpOutputDto } from '@application/dtos/authentication';
+import { Controller, HttpRequest, HttpResponse, OutputError } from '@presentation/shared/protocols';
+import { created, makeOutputErrorHelper } from '@presentation/shared/helpers';
+
+export class SignUpController implements Controller<SignUpOutputDto> {
+  constructor (
+    private readonly signUpUseCase: UseCase<SignUpInputDto, SignUpOutputDto>,
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<SignUpOutputDto | OutputError>> {
+    try {
+      const { body } = httpRequest;
+
+      const account = await this.signUpUseCase.exec(body);
+
+      return created(account);
+    } catch (error) {
+      return makeOutputErrorHelper(error as ErrorBase);
+    }
+  }
+}
