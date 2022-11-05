a6bea67ac2d3ce1b5ebc6630cf8226b2bd12e6ee
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-11-05 12:53:22 -0300

    feat: map errors

--- src/presentation/utils/output-error.ts
+++ src/presentation/utils/output-error.ts
@@ -3,6 +3,27 @@ import { badRequest, serverError } from '@presentation/utils';
 import { HttpResponse, OutputError } from '@presentation/protocols';
 
 export const makeOutputErrorHelper = (error: ErrorBase): HttpResponse<OutputError> => {
+  // Domain Errors
+  if (error.type === 'errors/common/invalidEmail') {
+    return badRequest(error);
+  }
+  if (error.type === 'errors/common/invalidId') {
+    return badRequest(error);
+  }
+  if (error.type === 'errors/common/invalidPassword') {
+    return badRequest(error);
+  }
+  if (error.type === 'errors/common/invalidUserName') {
+    return badRequest(error);
+  }
+
+  // Application Errors
+  if (error.type === 'errors/common/duplicatedEmail') {
+    return badRequest(error);
+  }
+
+
+  // Presentation Errors
   if (error.type === 'errors/common/invalidParameters') {
     return badRequest(error);
   }
