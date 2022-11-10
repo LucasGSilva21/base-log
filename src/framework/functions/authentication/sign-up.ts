4ad0af2f06faa34f2fb3aa90507c8e58cbd635d9
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-11-09 23:40:10 -0300

    feat: create makeLambdaHandler

--- src/framework/functions/authentication/sign-up.ts
+++ src/framework/functions/authentication/sign-up.ts
@@ -1,14 +1,7 @@
 import 'module-alias/register';
-import { APIGatewayEvent } from 'aws-lambda';
-import { LambdaHandlerResult } from '@framework/protocols';
+import { makeLambdaHandler } from '@framework/utils';
 import { makeSignUpController } from '@framework/factories/controllers/authentication';
 
-export const handler = async (event: APIGatewayEvent): Promise<LambdaHandlerResult> => {
-  const controller = makeSignUpController();
-  const data = JSON.parse(event.body);
-  const { statusCode, body } = await controller.handler({ body: data });
-  return {
-    statusCode,
-    body: JSON.stringify(body)
-  };
-};
+const controller = makeSignUpController();
+
+export const handler = makeLambdaHandler(controller);
