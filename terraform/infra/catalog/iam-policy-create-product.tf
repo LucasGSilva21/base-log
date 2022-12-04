f24f766a97f8c0a853e2a700f79f640daeaf0cdc
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-12-04 15:34:49 -0300

    fix: remove permission

--- terraform/infra/catalog/iam-policy-create-product.tf
+++ terraform/infra/catalog/iam-policy-create-product.tf
@@ -7,7 +7,6 @@ resource "aws_iam_policy" "baselog_create_product_policy" {
       {
         Action = [
           "dynamodb:PutItem",
-          "dynamodb:Query"
         ]
         Effect   = "Allow"
         Resource = "${aws_dynamodb_table.baselog_products.arn}"
