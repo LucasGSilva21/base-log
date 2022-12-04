3e6573e9782057f009f93e21dd32466b719c6c1e
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-12-04 15:09:15 -0300

    feat: add list product output dto

--- src/modules/catalog/application/dtos/index.ts
+++ src/modules/catalog/application/dtos/index.ts
@@ -1,2 +1,3 @@
 export * from './create-product-input-dto';
 export * from './create-product-output-dto';
+export * from './list-products-output-dto';
---
+++ src/modules/catalog/application/dtos/list-products-output-dto.ts
@@ -0,0 +1,5 @@
+import { ProductPrimitivesProps } from '@catalog/domain/entities';
+
+export interface ListProductsOutputDto {
+  products: ProductPrimitivesProps[]
+}
