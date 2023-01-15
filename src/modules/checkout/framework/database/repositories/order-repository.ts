a6aa69a3033a2bb227f65661c27a3ee45d8743e7
Lucas Gabriel <lucasgsilva2102@gmail.com>
2023-01-15 15:07:10 -0300

    feat: add find by id

--- src/modules/checkout/framework/database/repositories/order-repository.ts
+++ src/modules/checkout/framework/database/repositories/order-repository.ts
@@ -4,8 +4,27 @@ import { OrderRepository } from '@checkout/application/repositories';
 import { OrderModel } from '@checkout/framework/database/models';
 
 export class DynamodbOrderRepository implements OrderRepository {
+  async findById (id: Id): Promise<OrderEntity> {
+    const order = await OrderModel.query('id').eq(id.getValue()).exec();
+
+    if (order) {
+      return OrderEntity.create({
+        id: Id.create(order[0].id),
+        totalInCents: TotalInCents.create(order[0].totalInCents),
+        status: order[0].status,
+        amount: Amount.create(order[0].amount),
+        product: order[0].product,
+        transaction: order[0].transaction,
+        createdAt: order[0].createdAt,
+        updatedAt: order[0].updatedAt
+      });
+    }
+
+    return null;
+  }
+
   async create (data: OrderEntity): Promise<OrderEntity> {
-    const transaction = await OrderModel.create({
+    const order = await OrderModel.create({
       id: data.id.getValue(),
       totalInCents: data.totalInCents.getValue(),
       status: data.status,
@@ -15,14 +34,14 @@ export class DynamodbOrderRepository implements OrderRepository {
     });
 
     return OrderEntity.create({
-      id: Id.create(transaction.id),
-      totalInCents: TotalInCents.create(transaction.totalInCents),
-      status: transaction.status,
-      amount: Amount.create(transaction.amount),
+      id: Id.create(order.id),
+      totalInCents: TotalInCents.create(order.totalInCents),
+      status: order.status,
+      amount: Amount.create(order.amount),
       product: data.product,
       transaction: data.transaction,
-      createdAt: transaction.createdAt,
-      updatedAt: transaction.updatedAt
+      createdAt: order.createdAt,
+      updatedAt: order.updatedAt
     });
   }
 
